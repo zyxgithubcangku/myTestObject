96391921b9ad2a0b6c44356af931fea4b686a474
YuXing_Zhang <to_ave22@126.com>
2023-02-10 17:57:02 +0800

    再来

--- app/src/main/java/com/example/mytestobject/MainActivity.java
+++ app/src/main/java/com/example/mytestobject/MainActivity.java
@@ -10,8 +10,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //新的更新
-//        再来一次
-//        了就可怜见看来离开，l
     }
 }
